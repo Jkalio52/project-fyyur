aa0fb21fd8d65fbf743f389263a3f03bfa7a1556
Jay Kalio <jerrykalio@gmail.com>
2020-11-09 23:12:02 -0600

    Complete todos in config.py file.

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
